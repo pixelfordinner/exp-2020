ad7e57ebc244c1da4374552df93f58e4c4d16ad3
Julien Vergnaud <julien@pixelfordinner.com>
2019-12-17 12:07:21 -0300

    Draf&Flip behavior
    more control on interaction

--- assets/js/lib/components/interaction_flip/index.js
+++ assets/js/lib/components/interaction_flip/index.js
@@ -1,4 +1,3 @@
-
 import { Tools } from 'objects/tools/geometry'
 export class FlipInteraction {
   constructor (app, config = {}) {
@@ -19,16 +18,19 @@ export class FlipInteraction {
 
   initContainer () {
     this.container = this.config.object.container
-    console.log('hello', this.iObject)
+    // console.log('hello', this.iObject)
     this.container.tetha = 0
     this.container.startRotation = false
     this.container.endRotation = false
+    this.container.euler.y = 0
     this.container.isflipped = false
     this.container.goToRigth = 0
     this.container.numclicks = 0
     this.container.dragOrigin = new PIXI.projection.Point3d()
     this.container.currentPosition = new PIXI.projection.Point3d()
     this.container.totalSpeed = 0
+    this.container.totAngle = 0
+    this.container.mooving = false
     this.container.beginDrag = false
     this.container.on('pointerdown', this.onDragStart)
       .on('pointerup', this.onDragEnd)
@@ -39,8 +41,10 @@ export class FlipInteraction {
   onDragStart (event) {
     this.data = event.data
     this.alpha = 0.5
+    this.totAngle = 0
     this.dragging = true
     this.beginDrag = true
+    this.endRotation = false
     this.numclicks++
     this.dragOrigin = new PIXI.projection.Point3d(
       this.data.getLocalPosition(this.parent).x,
@@ -48,18 +52,26 @@ export class FlipInteraction {
       this.data.getLocalPosition(this.parent).z
     )
     this.currentPosition = this.dragOrigin
-    console.log(this.numclicks)
   }
 
   onDragEnd () {
     this.alpha = 1
     this.dragging = false
+    this.beginDrag = false
+    this.endRotation = false
     this.newDistance = Tools.getPolarlength(this.currentPosition, this.dragOrigin)
+    if (this.newDistance > 100) {
+      this.startRotation = true
+    } else {
+      this.dragging = false
+    }
   }
 
   onDragMove () {
     this.beginDrag = false
+
     if (this.dragging) {
+      this.mooving = true
       const newPosition = this.data.getLocalPosition(this.parent)
       if (newPosition.x < this.dragOrigin.x) {
         this.goToRigth = false
@@ -67,6 +79,7 @@ export class FlipInteraction {
         this.goToRigth = true
       }
       this.currentPosition = newPosition
+
       if (this.both) {
         this.distanceFromPrev = Tools.getPolarlength(newPosition, this.currentPosition)
       }
@@ -75,7 +88,7 @@ export class FlipInteraction {
       } else {
         this.distanceFromPrev = Tools.getXlength(newPosition, this.currentPosition)
       }
-      this.totalSpeed = 0.05
+      this.totalSpeed = Tools.map(this.distanceFromPrev, 5, 100, 0, 0.03)
     }
   }
 
@@ -85,32 +98,26 @@ export class FlipInteraction {
   }
 
   makeFlip () {
+    // reset animation speed
     this.speed = 0.0
-    console.log(Math.abs(Math.cos(this.container.euler.y)))
-
-    if (Math.cos(this.container.euler.y) > 0) {
-      this.container.isflipped = true
-    } else {
-      this.container.isflipped = false
-    }
-    if (this.container.dragging) {
+    // check container angle to see if we switch texture rendering
+    this.container.isflipped = Math.cos(this.container.euler.y) > 0
+    // check if we are dragging or if we already completed the drag moove
+    if (this.container.dragging && this.container.mooving || this.container.startRotation) {
       this.speed = this.container.totalSpeed
+      this.container.totAngle += this.container.totalSpeed
+    }
+    // check container angle to see if we completed the rotation
+    if (this.container.totAngle > Math.PI || Math.abs(Math.cos(this.container.euler.y)) > 0.99) {
+      this.container.startRotation = false
+      if (this.container.totAngle > Math.PI) this.speed = 0
+    } else if (this.container.totAngle > 0 && !this.dragging) {
+      console.log(this.container.totAngle)
+      // this.speed = -this.speed
+      // this.container.totAngle -= this.speed
+      // this.speed = -this.speed
+    }
 
-      this.dist = this.mouse.getMouseInfluenceX(this.container.dragOrigin, 0, 300, 0, 10)
-      console.log('yoyoy ', this.dist)
-      if (this.dist > 2) {
-        this.container.startRotation = true
-        this.container.dragging = false
-      }
-      this.dist = Tools.getPolarlength(this.mouse.pos, this.container.dragOrigin)
-    }
-    if (this.container.startRotation) {
-      this.speed = this.container.totalSpeed
-      if (Math.abs(Math.cos(this.container.euler.y)) >= 0.999) {
-        this.container.startRotation = false
-        this.container.dragging = false
-      }
-    }
     if (!this.container.beginDrag) {
       this.flip(this.container, this.speed)
     }
